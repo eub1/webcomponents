3feb50fe034f7a5b9c24548beba1fd641a9a5832
eub1 <ebonforte@gmail.com>
2023-05-18 12:44:04 -0300

    add animate on scroll component

---
+++ projects/animateOnScroll/script.js
@@ -0,0 +1,15 @@
+// This function will run when the visibility of any of the observed elements changes
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+
+hiddenElements.forEach((el) => observer.observe(el))
