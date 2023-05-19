82824b7c96a7be243a1835a461fc94fefb14f013
eub1 <ebonforte@gmail.com>
2023-05-18 23:38:26 -0300

    freecodecamp parallax

---
+++ projects/parallax/free_code_camp/main.js
@@ -0,0 +1,9 @@
+const parallax = document.getElementById('parallax')
+
+window.addEventListener('scroll', function () {
+  let offset = window.pageYOffset
+  // console.log('offset ' + offset)
+  // console.log('offset * 0.7' + offset * 0.7)
+
+  parallax.style.backgroundPositionY = offset * 0.8 + 'px'
+})
